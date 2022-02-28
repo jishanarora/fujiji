9087775ee70ec67573f1fd1942f674c92abdfc0a
Jishan Arora <jishan.sikasu@gmail.com>
2022-02-27 18:39:39 -0600

    Deployment

--- fujiji-server/src/app.js
+++ fujiji-server/src/app.js
@@ -1,49 +1,47 @@
 // Express
-const dotenv = require('dotenv');
-dotenv.config();
 
 const express = require('express');
-const { Sequelize } = require('sequelize');
-const { config, db } = require('./config/config');
+// const { Sequelize } = require('sequelize');
+// const { config} = require('./config/config');
 
 const app = express();
 
-const appUrl = config.APP_URL;
-const port = config.PORT || 3000;
+// const appUrl = config.APP_URL;
+const port = process.env.PORT || 3000;
 
-const sequelize = new Sequelize(db.NAME, db.USERNAME, db.PASSWORD, {
-  host: db.HOST,
-  dialect: 'mssql',
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000,
-  },
-});
+// const sequelize = new Sequelize(db.NAME, db.USERNAME, db.PASSWORD, {
+//   host: db.HOST,
+//   dialect: 'mssql',
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     idle: 10000,
+//   },
+// });
 
-app.get('/appstatus', async (req, res) => {
+app.get('/', async (req, res) => {
   const response = {
     message: 'Hello from the server!!!',
   };
 
-  const [allUser] = await sequelize.query('SELECT * FROM fujiji_user');
-  console.log(allUser);
-  const [allListing] = await sequelize.query('SELECT * FROM fujiji_listing');
-  console.log(allListing);
-  const [allToken] = await sequelize.query('SELECT * FROM fujiji_token');
-  console.log(allToken);
+  // const [allUser] = await sequelize.query('SELECT * FROM fujiji_user');
+  // console.log(allUser);
+  // const [allListing] = await sequelize.query('SELECT * FROM fujiji_listing');
+  // console.log(allListing);
+  // const [allToken] = await sequelize.query('SELECT * FROM fujiji_token');
+  // console.log(allToken);
 
-  return res.status(200).json({ response, allUser, allListing, allToken });
+  return res.status(200).json(response);
 });
 
 app.listen(port, async () => {
-  console.log(`Server is up on port ${appUrl}:${port}`);
+  console.log(`Server is up on port :${port}`);
   console.log('test');
 
-  try {
-    await sequelize.authenticate();
-    console.log('Connection has beena established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
+  // try {
+  //   await sequelize.authenticate();
+  //   console.log('Connection has beena established successfully.');
+  // } catch (error) {
+  //   console.error('Unable to connect to the database:', error);
+  // }
 });
